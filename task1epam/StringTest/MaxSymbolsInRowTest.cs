f0f6e65786e60d4c19865dbd40e97b97f4193127
Rubea1435 <andrew.bondarev.zeta@yandex.ru>
2021-07-16 04:16:52 +0300

    MaxNotSameSymbolsInRow method renamed in unit tests

--- task1epam/StringTest/MaxSymbolsInRowTest.cs
+++ task1epam/StringTest/MaxSymbolsInRowTest.cs
@@ -11,35 +11,35 @@ namespace StringTest
         public void EmptyStringTest()
         {
             string strEmpty = String.Empty;
-            Assert.AreEqual(0, strEmpty.MaxNotSameSymbolsInRow());
+            Assert.AreEqual(0, strEmpty.CalculateMaxNotSameSymbolsInRow());
         }
 
         [TestMethod]
         public void StringWith3DifferentSymbolsTest()
         {
             string str = "qwe";
-            Assert.AreEqual(3, str.MaxNotSameSymbolsInRow());
+            Assert.AreEqual(3, str.CalculateMaxNotSameSymbolsInRow());
         }
 
         [TestMethod]
         public void StringWith3SameSymbolsTest()
         {
             string str = "eee";
-            Assert.AreEqual(1, str.MaxNotSameSymbolsInRow());
+            Assert.AreEqual(1, str.CalculateMaxNotSameSymbolsInRow());
         }
 
         [TestMethod]
         public void StringWithAlternatingSymbolsTest()
         {
             string str = "121212";
-            Assert.AreEqual(6, str.MaxNotSameSymbolsInRow());
+            Assert.AreEqual(6, str.CalculateMaxNotSameSymbolsInRow());
         }
 
         [TestMethod]
         public void StringWithDifferentSymbolsStartingInsideTheStringTest()
         {
             string str = "qwee1234";
-            Assert.AreEqual(5, str.MaxNotSameSymbolsInRow());
+            Assert.AreEqual(5, str.CalculateMaxNotSameSymbolsInRow());
         }
     }
 }
