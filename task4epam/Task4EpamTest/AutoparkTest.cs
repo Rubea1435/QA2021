9925cb035c5112c94ae1d3f571d5b46d09d6188a
Rubea1435 <andrew.bondarev.zeta@yandex.ru>
2021-07-24 02:01:25 +0300

    task9 exceptions unit tests

--- task4epam/Task4EpamTest/AutoparkTest.cs
+++ task4epam/Task4EpamTest/AutoparkTest.cs
@@ -11,14 +11,54 @@ namespace Task4EpamTest
         static Bus scania = new BusBuilder().Create();
         static Car renault = new CarBuilder().Create();
         static Scooter honda = new ScooterBuilder().Create();
-        static Truck peterbilt = new TruckBuilder().Create();
+        static Truck peterbilt = new TruckBuilder().Create();    
         Autopark park = new Autopark(new List<Vehicle> { scania, renault, honda, peterbilt });
+
+        [TestInitialize]
+        public void SetID()
+        {
+            scania.ID = 1;
+            renault.ID = 2;
+            honda.ID = 3;
+            peterbilt.ID = 4;
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(AddException))]
+        public void AddExceptionTest()
+        {
+            Bus busWithSameID = new BusBuilder().Create();
+            busWithSameID.ID = 2;
+            park.AddVehicle(busWithSameID);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InitializationException))]
+        public void InitializationExceptionTest()
+        {
+            Car carWithNegativeMaxSpeed = new CarBuilder().Create();
+            carWithNegativeMaxSpeed.MaxSpeed = -10;
+        }
 
         [TestMethod]
         public void GetAutoByParameterTest()
         {
             var dieselVehicles = park.GetAutoByParameter(x => x.Engine.Type, EngineType.Diesel);
             CollectionAssert.AreEqual(new List<Vehicle> { scania, peterbilt }, dieselVehicles);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(UpdateAutoException))]
+        public void UpdateAutoExceptionTest()
+        {
+            park.UpdateVehicle(14, new CarBuilder().Create());
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(RemoveAutoException))]
+        public void RemoveAutoExceptionTest()
+        {
+            park.RemoveVehicle(14);
         }
     }
 }
