3e82b75139a139a5a92074283d9befc753b8d2b4
Rubea1435 <andrew.bondarev.zeta@yandex.ru>
2021-08-03 18:40:06 +0300

    new extensions

---
+++ task3epam/task3epam/Extensions/StringExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task3epam
+{
+    public static class StringExtensions
+    {
+        public static int CalculateMaxSameLatinLettersInRow(this string str)
+        {
+
+        }
+
+        public static int CalculateMaxSameDigitsInRow(this string str)
+        {
+
+        }
+    }
+}
